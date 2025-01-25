890a6c4707fc07782e95e78fda1876d69240a3c9
Ranidu Lankage <ranidulankage@ranidus-mbp-2.lan>
2025-01-25 14:34:55 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Pong from "./Pong";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Pong />
+    </div>
+  );
+}
+
+export default App;
